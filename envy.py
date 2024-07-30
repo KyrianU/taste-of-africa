a0350d939c1ac57856127bb84db38e1ada3dc5a0
Kyrian Uwaegbulem <kyrian.uwaegbulem@gmail.com>
2024-07-30 23:57:31 +0000

    added secret key and migrated database to elephantSQL

--- envy.py
+++
@@ -1,3 +0,0 @@
-import os 
-
-os.environ.setdefault("SECRET_KEY", "uol67i%xvy!)7k2dea(qsp0qfpizj848$w*@chasae6#2&-($v")
--- taste_of_africa/settings.py
+++ taste_of_africa/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b6_hoa6it6@f0ft3jb%nl-ry9%coh3!y+r$o!bva2@3z@n&adj'
+SECRET_KEY = 'ouc+50+q)offejp)n#arodbr-l)*%a2mxkd^9odto_ct&5@2e8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -73,11 +78,15 @@ WSGI_APPLICATION = 'taste_of_africa.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#    'default': {
+#       'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
