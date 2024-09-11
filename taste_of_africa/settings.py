7d96e6ad3d22c8e68e93a8acb7366738f68a8455
Kyrian Uwaegbulem <kyrian.uwaegbulem@gmail.com>
2024-09-11 01:34:01 +0000

    added imgages to the carousel element

--- taste_of_africa/settings.py
+++ taste_of_africa/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'ouc+50+q)offejp)n#arodbr-l)*%a2mxkd^9odto_ct&5@2e8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-kyrianu-tasteofafrica-uxgc193ybny.ws.codeinstitute-ide.net', 'localhost']
 
