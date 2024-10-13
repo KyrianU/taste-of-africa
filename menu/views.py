1c260c5750c7e6911269de11373f3311ca9a6a0f
Kyrian Uwaegbulem <kyrian.uwaegbulem@gmail.com>
2024-10-13 00:47:49 +0000

    added structured to the menu.html file

--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
 
 # Create your views here.
+from .models import Menu
+
+class MenuListView(ListView):
+    """
+    food menu as a list of items from the database
+    """
+    model = Menu
+    template = 'menu/menu.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
