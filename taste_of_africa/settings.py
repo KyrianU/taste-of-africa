a423a7b80a666fd62141b03d92da879c60fde985
Kyrian Uwaegbulem <kyrian.uwaegbulem@gmail.com>
2024-11-21 21:15:21 +0000

    changed DEBUG settings to True

--- taste_of_africa/settings.py
+++ taste_of_africa/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'ouc+50+q)offejp)n#arodbr-l)*%a2mxkd^9odto_ct&5@2e8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-kyrianu-tasteofafrica-uxgc193ybny.ws.codeinstitute-ide.net', 'localhost']
 
